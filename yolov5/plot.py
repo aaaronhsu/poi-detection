b081fe9aca54d4486df8e08445ab2f49f258cc7d
andrewjuang <andrewjuang01@gmail.com>
2023-06-04 17:48:47 -0400

    push plot

--- yolov5/plot.py
+++ yolov5/plot.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import optimize
 
 # open poi.txt file and read the content
 with open('./runs/tmp/poi/poi.txt', 'r') as f:
     content = f.readlines()
 
 # read only first 200 lines
-content = content[0:200]
+# content = content[0:20]
 
 content = [x.strip() for x in content]
 content = [x.split(',') for x in content]
@@ -15,7 +16,15 @@ content = [[float(x) for x in y] for y in content]
 data = np.array(content)
 x,y = data.T
 
-print(x)
+# x = cos(t) 
+# y = sin(t)
 
-plt.scatter(x,y)
-plt.show()
+# x, y
+def squared_dist(t, x, y):
+    return (x - np.cos(t))**2 + (y - np.sin(t))**2
+
+minimum = optimize.fmin(squared_dist, 0, (2,3))
+print(minimum[0])
+
+# plt.scatter(x,y)
+# plt.show()
