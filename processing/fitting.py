cdf2fb0ece5d8e68d28d5b9d9ce260bf71454c50
Aaron Hsu <aaron.hsu@duke.edu>
2023-06-10 22:31:31 -0400

    renamed variables

--- processing/fitting.py
+++ processing/fitting.py
@@ -1,25 +1,31 @@
 from scipy import optimize
 import numpy as np
 
-def generate_loss(parapoints, points):
+
+def generate_loss(poi_points, para_points):
     # find the minimum distance between points and the parametric curve stored in self.x_coords and self.y_coords
 
     loss = 0
 
-    for point in points:
+    for point in poi_points:
         # pass in parametric and point to find min distance
-        min_dist = find_min_distance(parapoints, point)
+        min_dist = find_min_distance(point, para_points)
         loss += min_dist[1]
 
     # print("Loss from", points, loss)
 
-    return loss / len(points)
+    return loss / len(poi_points)
 
-def evaluate_transformation(xtrans, ytrans, scale, points, parapoints):
-    parapoints = transform(parapoints, xtrans, ytrans, scale) # translate the parametric curve to the cemter of the points
-    current_loss = generate_loss(points, parapoints)
-    parapoints = inv_transform(parapoints, xtrans, ytrans, scale) # translate the parametric curve back to the origin
-    
+
+def evaluate_transformation(xtrans, ytrans, scale, poi_points, para_points):
+    para_points = transform(
+        para_points, xtrans, ytrans, scale
+    )  # translate the parametric curve to the cemter of the points
+    current_loss = generate_loss(poi_points, para_points)
+    para_points = inv_transform(
+        para_points, xtrans, ytrans, scale
+    )  # translate the parametric curve back to the origin
+
     return current_loss
 
 
@@ -27,33 +33,17 @@ def square_dist(x1, y1, x2, y2):
     return (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 
-def find_min_distance(parapoints, point):
+def find_min_distance(point, para_points):
     x = point[0]
     y = point[1]
-    min_dist = square_dist(parapoints[0][0], parapoints[0][1], x, y)
-    min_point = parapoints[0]
-    for pt in parapoints:
+    min_dist = square_dist(para_points[0][0], para_points[0][1], x, y)
+    min_point = para_points[0]
+    for pt in para_points:
         if square_dist(pt[0], pt[1], x, y) < min_dist:
             min_dist = square_dist(pt[0], pt[1], x, y)
             min_point = pt
 
     return [min_point], min_dist
-
-
-# this function is the same as the function above, but uses optimize.fmin which is less accurate
-def find_min_distance_opt(parametric, point):
-    # find the minimum distance between the parametric (function[0], function[1]) and the points (x, y)
-    # return the (values of t that minimizes the distance, the distance)
-    x = point[0]
-    y = point[1]
-
-    def squared_dist(t, x, y):
-        return square_dist(parametric.x(t), parametric.y(t), x, y)
-
-    # generate array of values of t that minimize the distance
-    minimum = optimize.fmin(squared_dist, 0, (x, y), disp=False)
-
-    return minimum, squared_dist(minimum, x, y)
 
 
 def calculate_centroid(points):
@@ -71,7 +61,7 @@ def transform(points, xtrans, ytrans, scale):
 
 def inv_transform(points, xtrans, ytrans, scale):
     # scale the points by scale, then translate by xtrans and ytrans
-    points = dilate(points, 1/scale)
+    points = dilate(points, 1 / scale)
     points = translate(points, -xtrans, -ytrans)
     return points
 
@@ -82,9 +72,10 @@ def translate(points, xtrans, ytrans):
     points[:, 1] += ytrans
     return points
 
+
 def dilate(points, scale):
     # scale the points by scale
-    x,y = calculate_centroid(points)
+    x, y = calculate_centroid(points)
     points = translate(points, -x, -y)
     points *= scale
     points = translate(points, x, y)
--- processing/main.py
+++ processing/main.py
@@ -7,16 +7,14 @@ import flowers
 import fitting
 
 # open the file that contains the coordinates of the poi
-file = FileReader("circle.txt")
+file = FileReader("antispin.txt")
 coordinates = file.read_file_content()  # read and parse the file content
 x_coords, y_coords = coordinates  # you can extract the coordinates like this
 
 # # this is how you can define a parametric curve
-parametric1 = Parametric(
-    flowers.gen_circle_x(0, 0.5), flowers.gen_circle_y(0,0.5)
-)
+parametric1 = Parametric(flowers.gen_circle_x(0, 0.5), flowers.gen_circle_y(0, 0.5))
 parametric2 = Parametric(
-    flowers.gen_antispin_x(5, 1, 4), flowers.gen_antispin_y(5, 1, 4)
+    flowers.gen_antispin_x(5, 0.3, 4), flowers.gen_antispin_y(5, 0.3, 4)
 )
 
 # # this is how you can plot the parametric curve
@@ -34,7 +32,7 @@ parametric2 = Parametric(
 # graph.create_graph(parametric1.coords)
 # graph.create_graph(file.coords)
 
-fitted_coords = parametric1.fit(file.coords)
-graph.create_graph_multiple([file.coords, fitted_coords[1], parametric1.coords])
+fitted_coords = parametric2.fit(file.coords)
+graph.create_graph_multiple([file.coords, fitted_coords[1], parametric2.coords])
 
 # print(parametric1.coords)
