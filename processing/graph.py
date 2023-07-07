2b7fdc4b2b5e8bad660a53a0009f651b142b6215
Aaron Hsu <aaron.hsu@duke.edu>
2023-07-07 01:36:36 -0400

    graph.py works with point class

---
+++ processing/graph.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from point import Point
+
+
+def create_graph(points: list[Point]) -> None:
+    x_coords: list[float] = []
+    y_coords: list[float] = []
+
+    for point in points:
+        x_coords.append(point.x)
+        y_coords.append(point.y)
+
+    plt.scatter(x_coords, y_coords)
+    plt.show()
+
+
+def create_graph_multiple(*point_list: list[Point]) -> None:
+    colors: list[str] = [
+        "red",
+        "blue",
+        "green",
+        "purple",
+        "orange",
+        "yellow",
+        "pink",
+        "black",
+    ]
+
+    point_list_num: int = 0
+
+    for points in point_list:
+        x_coords: list[float] = []
+        y_coords: list[float] = []
+
+        for point in points:
+            x_coords.append(point.x)
+            y_coords.append(point.y)
+
+        plt.scatter(x_coords, y_coords, color=colors[point_list_num % len(colors)])
+        point_list_num += 1
+
+    plt.show()
