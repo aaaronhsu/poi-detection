36ef99d2bc411bebfcab5c04d4bdd3b30b97dccf
Aaron Hsu <aaron.hsu@duke.edu>
2023-07-07 01:30:59 -0400

    file reader generates point objects

---
+++ processing/file_reader.py
@@ -0,0 +1,24 @@
+from point import Point
+
+
+class FileReader:
+    def __init__(self, filename) -> None:
+        self.filename: str = filename
+        self.coords: list[Point] = []
+        self.file_content: list = []
+
+    def read_file_content(self) -> list[Point]:
+        with open("processing/" + self.filename, "r") as f:
+            self.file_content = f.readlines()
+        return self.parse_file_content()
+
+    def parse_file_content(self) -> list[Point]:
+        self.file_content = [x.strip() for x in self.file_content]
+        self.file_content = [x.split(",") for x in self.file_content]
+        self.file_content = [[float(x) for x in y] for y in self.file_content]
+
+        # shape of coords is (n, 2)
+        for x, y in self.file_content:
+            self.coords.append(Point(x, y))
+
+        return self.coords
