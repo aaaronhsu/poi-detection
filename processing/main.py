ba790d775c53b5188175a0ff737f091f90694de3
Aaron Hsu <aaron.hsu@duke.edu>
2023-07-09 23:23:49 -0400

    Revert "created video parser that can iterate through frames and detect objects"
    This reverts commit 0353ddbfc3567f4da196412c5276a20b56bd791d.

--- processing/main.py
+++ processing/main.py
@@ -1,31 +1,10 @@
 import numpy as np
 from file_reader import FileReader
-from video_parser import VideoParser
 import graph
 
-video = VideoParser("processing/model/poi_short.mp4")
-images = video.parse_video(2)  # parse at 2 frames a second
+file = FileReader("test.txt")
 
-video_data = video.detect_objects()
+best_parametric = file.fit_all(1)
 
-for frame in video.frames:
-    print(
-        "frame",
-        frame.number,
-        "poi:",
-        frame.poi_coordinates,
-        "hand:",
-        frame.hand_coordinates,
-    )
-
-# poi_coordinates, hand_coordinates = video.extract_object_coordinates()
-
-# print(poi_coordinates)
-# print(hand_coordinates)
-
-# file = FileReader("test.txt")
-
-# best_parametric = file.fit_all(1)
-
-# print("Best fit:", best_parametric.type)
-# graph.create_graph(file.points, best_parametric.points)
+print("Best fit:", best_parametric.type)
+graph.create_graph(file.points, best_parametric.points)
