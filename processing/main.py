40122e53bbc1c0e11685186b3a6eaf75980ac74f
Aaron Hsu <aaron.hsu@duke.edu>
2023-07-14 17:10:51 -0400

    video overlay

--- processing/main.py
+++ processing/main.py
@@ -3,15 +3,18 @@ from file_reader import FileReader
 from video_parser import VideoParser
 import output
 
-video = VideoParser("processing/model/antispin.mp4")
-images = video.parse_video(10)  # parse at 10 frames a second
+video_path: str = "processing/model/antispin.mp4"
 
-video_data = video.detect_objects()
-video.export_coordinates("poi_short.txt")
+# video = VideoParser(video_path)
+# images = video.parse_video(10)  # parse at 10 frames a second
+
+# video_data = video.detect_objects()
+# video.export_coordinates("poi_short.txt")
 
 file = FileReader("poi_short.txt")
 
 best_parametric = file.fit_all(5)
 
 print("Best fit:", best_parametric.type)
-output.create_graph(file.points, best_parametric.points)
+# output.create_graph(file.points, best_parametric.points)
+output.overlay_video(video_path, best_parametric.points)
--- processing/output.py
+++ processing/output.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from point import Point
+import cv2
 
 
 def create_graph(*point_list: list[Point]) -> None:
@@ -33,4 +34,51 @@ def create_graph(*point_list: list[Point]) -> None:
 
 def overlay_video(video_path: str, *point_list: list[Point]) -> None:
     # overlay the video with the parametric curve
-    pass
+    cap = cv2.VideoCapture(video_path)
+
+    colors: list[str] = [
+        "red",
+        "blue",
+        "green",
+        "purple",
+        "orange",
+        "yellow",
+        "pink",
+        "black",
+    ]
+
+    try:
+        frames: int = int(cap.get(cv2.cv.CAP_PROP_FRAME_COUNT))
+        width: int = int(cap.get(cv2.cv.CAP_PROP_FRAME_WIDTH))
+        height: int = int(cap.get(cv2.cv.CAP_PROP_FRAME_HEIGHT))
+    except AttributeError:
+        frames: int = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        width: int = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height: int = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fig, ax = plt.subplots(1, 1)
+    plt.ion()
+    plt.show()
+
+    for i in range(frames):
+        point_list_num: int = 0
+        fig.clf()
+        flag, frame = cap.read()
+
+        plt.imshow(frame)
+
+        for points in point_list:
+            x_coords: list[float] = []
+            y_coords: list[float] = []
+
+            for point in points:
+                x_coords.append(point.x)
+                y_coords.append(point.y)
+
+            plt.scatter(x_coords, y_coords, color=colors[point_list_num % len(colors)])
+            point_list_num += 1
+
+        plt.pause(0.01)
+
+        if cv2.waitKey(1) == 27:
+            break
